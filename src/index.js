17e200234c3c21d0b4488f561b2d2847132faabf
Maureen Njihia <maureennjihia468@gmail.com>
2022-08-27 23:54:08 +0300

    Fetch first movie details

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // Your code here
+
+const filmsUrl = " http://localhost:3000/films";
+
+//Fetch fist movie details
+function fetchFirstMovie (id) {
+    fetch(`${filmsUrl}/${id}`)
+    .then(response => response.json())
+    .then(movie => {
+        document.getElementById("poster").src = movie.poster;
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetchFirstMovie(1);
+})
